c11420a22174d55244e956fe6fdae1d4528e76f8
Llym <1448189829@qq.com>
2020-02-23 17:22:47 +0800

    ....

---
+++ c++/PTA——汽车加油问题.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int max_num = 9999;
+int n, k;
+int *dis;
+
+void drive(int exist_gas, int aim_station, int num)
+{
+    if (aim_station - 1 == k + 1)
+    {
+        if (num < max_num)
+            max_num = num;
+        return;
+    }
+    if (exist_gas < dis[aim_station] || num >= max_num)
+        return;
+    drive(exist_gas - dis[aim_station], aim_station + 1, num);
+    drive(n, aim_station + 1, num + 1);
+}
+
+int main()
+{
+
+    cin >> n >> k;
+    dis = new int[k + 2]();
+    for (int i = 1; i <= k + 1; ++i)
+    {
+        cin >> dis[i];
+    }
+    drive(n, 1, 0);
+    if (max_num != 9999)
+        cout << max_num;
+    else
+        cout << "No Solution!";
+    return 0;
+}
