16ad2cba67764177dec81529f433a43b214203fb
Llym <1448189829@qq.com>
2020-03-03 21:34:04 +0800

    .....

---
+++ c++/PTA——Treap区间最大连续子段求和.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> nums(n + 1);
+
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> nums[i];
+    }
+    int p, kl, xr, sum, result;
+    for (int i = 0; i < m; ++i)
+    {
+
+        cin >> p >> kl >> xr;
+        if (p == 1)
+        {
+            nums.insert(nums.begin() + kl + 1, xr);
+        }
+        else if (p == 2)
+        {
+            sum = nums[kl];
+            result = -999999;
+            for (int j = kl + 1; j <= xr; ++j)
+            {
+                sum = max(nums[j], sum + nums[j]);
+                result = max(result, sum);
+            }
+            cout << result << endl;
+        }
+    }
+    return 0;
+}
